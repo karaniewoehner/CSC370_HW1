130fd2d858269bfdb32db119a6a051f2cf45fc68
taumurerwakalisa <taumurerwakalisa@davidson.edu>
2026-09-18 20:31:47 -0400

    Add few changes to branching-factor file

--- CSC370_BranchingFactor.py
+++ CSC370_BranchingFactor.py
@@ -1,21 +1,21 @@
 # Effective branching factor
 ## Calculate a full tree of depth d
-def total_nodes_for_b(b, d):
+def tree_size(b, depth):
     total = 0
-    for power in range(d + 1):
+    for power in range(depth + 1):
         total += b ** power
     return total
 
 ## Compare tree size against our target value of N + 1 until we reach it,
 ## then return our effective branching factor
-def effective_b_factor(N, d):
+def effective_b_factor(N, depth):
     # base case
-    if d == 0:
+    if depth == 0:
         return 0.0
     target = N + 1
     b = 1.0
     step = 0.01
-    while total_nodes_for_b(b, d) < target:
+    while tree_size(b, depth) < target:
         b += step
     return b
 
